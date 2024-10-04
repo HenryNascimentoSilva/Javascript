9a6e84e0c160b61099ac03837a0d22c1f658fa1b
henrynascimentosilva <henrynascimentosilva@gmail.com>
2024-10-04 18:16:00 -0300

    Média Aritmética

---
+++ media.js
@@ -0,0 +1,10 @@
+let media = [2,4,2,4,2,4]
+let resultado = 0;
+
+for (let i = 0; i < media.length; i++) {
+  resultado += media[i];
+}
+
+resultado = resultado / media.length;
+
+console.log(resultado);
