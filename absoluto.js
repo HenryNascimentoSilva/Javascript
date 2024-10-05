0680f517319723c26d9600251ffa2856ad603fb4
henrynascimentosilva <henrynascimentosilva@gmail.com>
2024-10-05 16:45:00 -0300

    Número Absoluto

---
+++ absoluto.js
@@ -0,0 +1,8 @@
+function valorAbsoluto(num) {
+  if (num < 0) return -num;  
+  else return num;
+}
+
+console.log(valorAbsoluto(-3));
+console.log(valorAbsoluto(-2));
+console.log(valorAbsoluto(4));
